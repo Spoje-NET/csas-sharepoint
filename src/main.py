bb8cc4149a2470eee20c1863a9826d1f327369f4
CyberVitexus <info@vitexsoftware.cz>
2025-09-16 11:37:43 +0200

    Removes obsolete script for SharePoint file uploads
    Deletes a script responsible for downloading statements and uploading ABO files to SharePoint. The functionality is no longer needed, simplifying the codebase and removing unnecessary dependencies.

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# Created on : Sep 16, 2025, 10:43:48 AM
+# Author     : Vítězslav Dvořák <info@vitexsoftware.cz>
+
+
+# This is a sample Python script.
+
+# Press Shift+F6 to execute it or replace it with your code.
+
+
+def print_hi(name):
+    print(f"Hi, {name}")
+
+
+if __name__ == "__main__":
+    print_hi("Netbeans")
